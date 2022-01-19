f90ccd19aababa4429acda9a17470611565a5712
Aleksi Nokela <n0noal00@students.oamk.fi>
2022-01-19 10:18:32 +0200

    Pushing the code to remote repository

---
+++ Styles.js
@@ -0,0 +1,36 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 30,
+    },
+        header: {
+        textAlign: 'center',
+        fontSize: 30,
+        fontWeight:'bold',
+    },
+    field: {
+        margin: 10,
+    },
+    input: {
+        marginLeft: 10,
+    },
+    radio: {
+        marginTop: 10,
+        marginBottom: 10,
+    },  
+    text_green: {
+        color:'green',
+        fontSize: 25,
+    },
+    text_yellow: {
+        color:'orange',
+        fontSize: 25,
+    },
+    text_red: {
+        color:'red',
+        fontSize: 25,
+    },
+  
+  });
